5b96be32df047446a17da60351afb4c89197bdf1
Kyle McLean <kmcle7zk@gmail.com>
2018-04-23 22:03:42 -0400

    Added initial controller logic for viewing and saving orders.

--- src/Web/Controllers/OrdersController.cs
+++ src/Web/Controllers/OrdersController.cs
@@ -1,22 +1,45 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetStore.Web.Interfaces;
+using PetStore.Web.Models.Entities;
 
 namespace PetStore.Web.Controllers
 {
     [Route("api/[controller]")]
     public class OrdersController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateOrder()
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
         {
-            // Stub
-            return Ok();
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] Order order)
+        {
+            // Ensure there were no binding issues.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var savedOrder = await _orderService.SaveOrderAsync(order);
+
+            return Ok(savedOrder);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetOrder(long id)
+        public async Task<IActionResult> GetOrder(long id)
         {
-            // Stub
-            return Ok();
+            var order = await _orderService.GetOrderAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
         }
     }
 }
